c94eb28029ce210067160b578249749875b6a314
unknown <Jose@.(none)>
2011-06-05 13:54:22 +0200

    iniciacion del proyecto (vamos a recordar, los commit descriptivos) Signed-off-by: unknown <Jose@.(none)>

---
+++ principal/Main.java
@@ -0,0 +1,15 @@
+package principal;
+
+import com.badlogic.gdx.backends.jogl.JoglApplication;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new JoglApplication(new Game(), "uhEducation", 800, 600, true);
+
+	}
+
+}
