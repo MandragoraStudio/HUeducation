a16e6d13d11d718299dd8f96aeb0ec4c114c2800
unknown <Jose@.(none)>
2011-06-19 18:09:51 +0200

    cambiado screen manager Signed-off-by: unknown <Jose@.(none)>

--- principal/ScreenManager.java
+++ principal/ScreenManager.java
@@ -1,5 +1,40 @@
 package principal;
 
+import java.util.Map;
+
 public class ScreenManager {
 
+	public static ScreenManager getScreenManager;
+	public Screen currentScreen;
+	public Map<String,Screen> screens;
+	
+	private ScreenManager(){
+		
+	}
+	
+	{
+		getScreenManager = new ScreenManager();
+		
+	}
+
+	public Screen getCurrentScreen() {
+		return currentScreen;
+	}
+
+	public void setCurrentScreen(Screen currentScreen) {
+		this.currentScreen = currentScreen;
+	}
+
+	public Map<String, Screen> getScreens() {
+		return screens;
+	}
+
+	public void setScreens(Map<String, Screen> screens) {
+		this.screens = screens;
+	}
+
+	public void addScreen(String cad, Screen s){
+		this.screens.put(cad, s);
+	}
+	
 }
