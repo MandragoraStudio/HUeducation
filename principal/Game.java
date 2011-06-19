d44df2e0dc245db2f79b9951157a0e92427dd33e
Antonio Garcia <thankor@gmail.com>
2011-06-19 17:57:09 +0200

    a

--- principal/Game.java
+++ principal/Game.java
@@ -5,13 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Mesh;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 
 public class Game implements ApplicationListener {
 	private Mesh mesh;
-	private Texture textura;
 
 	@Override
 	public void create() {
@@ -24,8 +22,7 @@ public class Game implements ApplicationListener {
 				Color.toFloatBits(0, 255, 0, 255), 1, 1, 0, 0.5f, 0,
 				Color.toFloatBits(0, 0, 255, 255), 0.5f, 0 });
 		mesh.setIndices(new short[] { 0, 1, 2 });
-		textura = new Texture(Gdx.files.internal("img/badlogic.jpg"));
-
+		
 	}
 
 	@Override
@@ -45,7 +42,7 @@ public class Game implements ApplicationListener {
 		//Gdx.gl.glClearColor(0, 1, 0, 1);
 	      Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);   
 	      Gdx.graphics.getGL10().glEnable(GL10.GL_TEXTURE_2D);
-	      textura.bind();
+	      
 	      mesh.render(GL10.GL_TRIANGLES, 0, 3);   
 	}
 
