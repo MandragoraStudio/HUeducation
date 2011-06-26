d3f8e42f377a83bdbee682ba06b6f73e0186c972
unknown <Jose@.(none)>
2011-06-26 17:09:56 +0200

    tiempo en la clase de carga
    Signed-off-by: unknown <Jose@.(none)>

--- principal/MandragoraSplash.java
+++ principal/MandragoraSplash.java
@@ -1,20 +1,29 @@
 package principal;
 
+import java.util.Calendar;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.actors.Image;
 
 public class MandragoraSplash extends Screen {
 
-	public boolean iniciado = false;
+	private boolean iniciado = false;
 	CargaInicio c;
+	private Calendar calendario;
+	private long tiempoini=0;
+	private long tiempoaux=0;
+	private long aux=0;
+	private long fin=5000;
 
 	@Override
 	public void Initialize(){
 		super.Initialize();
 
 		Image i = new Image("imagen", new Texture("imagenes/mandragora800.png"));
-
+		calendario = Calendar.getInstance();
 		this.escena.addActor(i);
+		
+		tiempoini=System.currentTimeMillis();
 	}
 
 	@Override
@@ -26,9 +35,10 @@ public class MandragoraSplash extends Screen {
 			iniciado = true;
 		}
 		// aqui el update de la pantalla
-
+		tiempoaux = System.currentTimeMillis();
+		aux=tiempoaux-tiempoini;
 		// finalmente pasamos a la pantalla EducationSplash
-		if (c.terminado) {
+		if (c.terminado && fin<=aux) {
 			ScreenManager.getScreenManager().setCurrentScreen("esplash");
 		}
 	}
