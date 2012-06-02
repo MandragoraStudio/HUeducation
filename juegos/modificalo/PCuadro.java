2cd556c9d23acbca10034c482ed04d160b0b833c
unknown <2Dilos-Eve@.(none)>
2012-06-02 17:46:27 +0200

    Signed-off-by: unknown <2Dilos-Eve@.(none)> Ampliada la zona de colocacion de los objetos Signed-off-by: unknown <2Dilos-Eve@.(none)>

--- juegos/modificalo/PCuadro.java
+++ juegos/modificalo/PCuadro.java
@@ -15,16 +15,16 @@ public class PCuadro {
 	
 	public PCuadro (int c) {
 		if (c == 1) {
-			uGafa = new Ubicacion("gafa", 384, 437, 80);
-			uBigote = new Ubicacion("bigote", 410, 397, 100);
-			uPeluca = new Ubicacion("peluca", 387, 312, 80);
-			uCollar = new Ubicacion("collar", 405, 295, 80);
-			uObjeto = new Ubicacion("objeto", 300, 112, 80);
+			uGafa = new Ubicacion("gafa", 384, 437, 110);
+			uBigote = new Ubicacion("bigote", 410, 397, 120);
+			uPeluca = new Ubicacion("peluca", 387, 312,100);
+			uCollar = new Ubicacion("collar", 405, 295, 100);
+			uObjeto = new Ubicacion("objeto", 300, 112, 100);
 		}
 		if (c == 2) {
-			uGafa = new Ubicacion("gafa", 388, 400, 50);
-			uPeluca = new Ubicacion("peluca", 398, 288, 80);
-			uObjeto = new Ubicacion("objeto", 399, 118, 80);
+			uGafa = new Ubicacion("gafa", 388, 400, 80);
+			uPeluca = new Ubicacion("peluca", 398, 288, 110);
+			uObjeto = new Ubicacion("objeto", 399, 118, 110);
 		}
 	}
 }
