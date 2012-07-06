169a7d0852e7ed36008702a40f847b06f2a703fe
spartanvs <amadorolivaresjuanmanuel@gmail.com>
2012-07-06 21:37:00 +0200

    Signed-off-by: spartanvs <amadorolivaresjuanmanuel@gmail.com>

--- principal/EducationSplash.java
+++ principal/EducationSplash.java
@@ -10,16 +10,9 @@ import com.badlogic.gdx.scenes.scene2d.actors.Image;
 
 public class EducationSplash extends Screen {
 	
-	private long ultimotiempo=0;
-	private long changetime=200;
+	
 	private int i = 0;
 	private int j = 0;
-	
-	// Ardilla Hablando
-	private Image[] A = new Image[3];	
-	private int MAXimages = 3;
-	private int posX = 700;
-	private int posY = 0;
 
 	private Music[] take = new Music[10];
 	int indice = 0;
@@ -30,27 +23,27 @@ public class EducationSplash extends Screen {
 	Image cuento;
 	Image modificalo;
 	Image museo;
-	Image mezcla;	
+	Image mezcla;
 
 	@Override
 	public void Update(){
 		// Animacion ardilla
-		if(System.currentTimeMillis() - ultimotiempo >= changetime){
-			ultimotiempo = System.currentTimeMillis();
+		if(System.currentTimeMillis() - GameGlobals.ultimotiempo >= GameGlobals.changetime){
+			GameGlobals.ultimotiempo = System.currentTimeMillis();
 			i++;
-			if(i >= MAXimages){
+			if(i >= GameGlobals.MAXimages){
 				i = 0;
 			}
-			A[i].x = posX;
-			A[i].y = posY;
+			GameGlobals.A[i].x = GameGlobals.posXardilla;
+			GameGlobals.A[i].y = GameGlobals.posYardilla;
 			// El resto aparecen invisibles
 			for(j = 0; j < i; j++){
-				A[j].x = this.escena.width();
-				A[j].y = this.escena.height();
+				GameGlobals.A[j].x = this.escena.width();
+				GameGlobals.A[j].y = this.escena.height();
 			}
-			for(j = i+1; j < MAXimages; j++){
-				A[j].x = this.escena.width();
-				A[j].y = this.escena.height();
+			for(j = i+1; j < GameGlobals.MAXimages; j++){
+				GameGlobals.A[j].x = this.escena.width();
+				GameGlobals.A[j].y = this.escena.height();
 			}
 		}
 		
@@ -121,22 +114,20 @@ public class EducationSplash extends Screen {
 		
 		Image i = new Image("img",new TextureRegion(new Texture("imagenes2/splash/esplash.png"), 0, 0, 1024, 600));
 		this.escena.addActor(i);
-		ultimotiempo=System.currentTimeMillis();
+		GameGlobals.ultimotiempo=System.currentTimeMillis();
 		
 		// Imagenes de las ardillas hablando
-		A[0] = new Image("mandrilla1",new TextureRegion(new Texture("imagenes2/ardilla/hablando/mandrilla1.png")));
-		this.escena.addActor(A[0]);
-		A[1] = new Image("mandrilla2",new TextureRegion(new Texture("imagenes2/ardilla/hablando/mandrilla2.png")));
-		this.escena.addActor(A[1]);
-		A[2] = new Image("mandrilla3",new TextureRegion(new Texture("imagenes2/ardilla/hablando/mandrilla3.png")));
-		this.escena.addActor(A[2]);
-		A[0].x = posX;
-		A[0].y = posY;
+		this.escena.addActor(GameGlobals.A[0]);
+		this.escena.addActor(GameGlobals.A[1]);
+		this.escena.addActor(GameGlobals.A[2]);
+		
+		GameGlobals.A[0].x = GameGlobals.posXardilla;
+		GameGlobals.A[0].y = GameGlobals.posYardilla;
 		// La 2 y la 3 aparecen inicialmente invisibles
-		A[1].x = this.escena.width();
-		A[1].y = this.escena.height();
-		A[2].x = this.escena.width();
-		A[2].y = this.escena.height();
+		GameGlobals.A[1].x = this.escena.width();
+		GameGlobals.A[1].y = this.escena.height();
+		GameGlobals.A[2].x = this.escena.width();
+		GameGlobals.A[2].y = this.escena.height();
 		
 		// Sonidos de la ardilla hablando
 		take[0] = Gdx.audio.newMusic(Gdx.files.internal("sonido/vocesdemandrilla/takesdelinicio/Take 1.wav"));
