8a4dabed8ff2e1846ae5a5074019145a33389b89
Antonio Garcia <thankor@gmail.com>
2012-07-09 21:26:44 +0200

    Signed-off-by: Antonio Garcia <thankor@gmail.com>

--- juegos/modificalo/SeleccionCuadro.java
+++ juegos/modificalo/SeleccionCuadro.java
@@ -4,17 +4,20 @@ import principal.ScreenManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.actors.Button;
+import com.badlogic.gdx.scenes.scene2d.actors.Image;
 import com.badlogic.gdx.scenes.scene2d.actors.Button.ClickListener;
 
 import juegos.Juego;
 
 public class SeleccionCuadro extends Juego{
 	
+	Image fondo;
 	Button p1;
 	Button p2;
 	
 	@Override
 	public void Load(){
+		fondo = new Image("fondo",new TextureRegion(new Texture("imagenes2/modificalo/elemCuadro3.png"),0,0,1024,600));
 		p1 = new Button("btn1",new TextureRegion(new Texture("imagenes2/modificalo/elemCuadro2.png"),0,0,318,484));
 		p1.clickListener=new ClickListener(){
 			public void clicked(Button arg0) {	
@@ -46,7 +49,7 @@ public class SeleccionCuadro extends Juego{
 		
 		p2.x = 550;
 		p2.y = 60;
-		
+		this.escena.addActor(fondo);
 		this.escena.addActor(p1);
 		this.escena.addActor(p2);
 	}
